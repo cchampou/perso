4a2bca774ef3685796a89e64b2ceb231deb655e6
Clement Champouillon <clement@champouillon.com>
2025-08-16 16:25:05 +0200

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   site: "https://clement.champouillon.com",
   outDir: "docs",
+  integrations: [
+    sitemap({
+      lastmod: new Date(),
+    })
+  ],
 });
