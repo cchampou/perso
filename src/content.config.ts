7d5bbef26acc6a0926b72ada3f2e1285de592153
cchampou <clement@champouillon.com>
2025-12-31 08:38:00 +0100

    fix: social media headers

--- src/content.config.ts
+++ src/content.config.ts
@@ -24,6 +24,7 @@ const blog = defineCollection({
   schema: ({ image }) =>
     z.object({
       title: z.string(),
+      description: z.string(),
       date: z.date(),
       cover: image(),
     }),
